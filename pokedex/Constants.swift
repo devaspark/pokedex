ed6d03ef799679f9c5c02104e9540794edde84db
rkung <rex.h.kung@gmail.com>
2017-11-21 22:08:56 -0700

    adding alamofire code

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Rex Kung on 11/21/17.
+//  Copyright © 2017 Rex Kung. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -7,10 +7,18 @@
 //
 
 import Foundation
+import Alamofire
 
 class Pokemon {
-    fileprivate var _name: String!
-    fileprivate var _pokedexId: Int!
+    private var _name: String!
+    private var _pokedexId: Int!
+    private var _description: String!
+    private var _defese: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolutionText: String!
+    private var _pokemonURL: String!
     
     var name: String {
         
@@ -26,5 +34,14 @@ class Pokemon {
         
         self._name = name
         self._pokedexId = pokedexId
+        
+        self._pokemonURL="\(URL_BASE)\(URL_POKEMON)\(self.pokedexId)/"
+    }
+    
+    func downloadPokemonDetail(completed: @escaping DownloadComplete) {
+        Alamofire.request(_pokemonURL).responseJSON { (response) in
+            print(response.result.value)
+        }
+        
     }
 }
